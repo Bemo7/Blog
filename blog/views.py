d2fd9d932b70c095425d709abcbca7031a9254d2
BEMO <82284405+Jaggzy7@users.noreply.github.com>
2022-12-15 00:12:08 +0100

    urls & initial views constructed

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def starting_page(request):
+    return
+
+def posts(request):
+    return
+
+def post_info(request):
+    return
